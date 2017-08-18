2e58c4b32a36e8c1d60cf5cb7d13f778cf847a10
dshire <wellereitetclub@gmail.com>
2017-08-18 16:30:39 +0200

    added login and registration functions

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(require('body-parser').urlencoded({
 const spicedPg = require('spiced-pg');
 const secrets = require('./secrets.json');
 const db = spicedPg(`postgres:${secrets.dbUser}:${secrets.dbPass}@localhost:5432/petition`);
+const bcrypt = require('./bcrypt.js');
 
 var hb = require('express-handlebars');
 app.engine('handlebars', hb({defaultLayout: 'main'}));
@@ -22,31 +23,50 @@ app.use(cookieSession({
 app.use(require('cookie-parser')());
 
 app.use(function cookieCheck(req, res, next) {
-    if (req.session.sigId && req.url == '/') {
+    if (req.session.user && req.url == '/') {
+        res.redirect('/sign');
+    }  else if (req.session.user && req.session.user.sigId && req.url == '/sign') {
         res.redirect('/signed');
-    }  else {
+    } else {
         next();
     }
 });
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-    res.render('petition', {
-    });
+app.get('/', function (req, res){
+    res.render('signup', {});
+});
+
+app.get('/login', function(req, res) {
+    if (req.session.user) {
+        res.redirect('/sign');
+    } else {
+        res.render('login', {});
+    }
+});
+
+app.get('/sign', function (req, res) {
+    if (req.session.user) {
+        res.render('petition', {
+            name: req.session.user.first + ' ' + req.session.user.last
+        });
+    } else {
+        res.redirect('/');
+    }
 });
 
 app.get('/signed', function (req, res) {
     let numSig = 0;
-    if (req.session.sigId) {
+    if (req.session.user.sigId) {
         db.query(`SELECT COUNT(*) FROM signatures`).then(function(result) {
             numSig = result.rows[0].count;
-
         }).then(function(){
-            db.query(`SELECT signature FROM signatures WHERE id = ${req.session.sigId}`).then(function(result){
+            db.query(`SELECT signature FROM signatures WHERE id = ${req.session.user.sigId}`).then(function(result){
                 res.render('signed', {
                     img: result.rows[0].signature,
-                    numSig: numSig
+                    numSig: numSig,
+                    name: req.session.user.first + ' ' + req.session.user.last
                 });
             });
         }).catch(function(err) {
@@ -57,18 +77,19 @@ app.get('/signed', function (req, res) {
     }
 });
 
-app.get('/sign', (req, res) => {
+app.get('/signAgain', (req, res) => {
     let numSig = 0;
-    if (req.session.sigId) {
+    if (req.session.user.sigId) {
         db.query(`SELECT COUNT(*) FROM signatures`).then(function(result) {
             numSig = result.rows[0].count;
 
         }).then(function(){
 
-            db.query(`SELECT signature FROM signatures WHERE id = ${req.session.sigId}`).then(function(result){
-                res.render('sign', {
+            db.query(`SELECT signature FROM signatures WHERE id = $1`, [req.session.user.sigId]).then(function(result){
+                res.render('signAgain', {
                     img: result.rows[0].signature,
-                    numSig: numSig
+                    numSig: numSig,
+                    name: req.session.user.first + ' ' + req.session.user.last
                 });
             });
         }).catch(function(err) {
@@ -76,29 +97,12 @@ app.get('/sign', (req, res) => {
         });
     } else {
         res.redirect('/');
-    }
-});
-
-app.post('/sign', (req, res) => {
-    if (req.body.sig.length > 0) {
-
-        db.query(`UPDATE signatures SET signature = $1 WHERE id = $2`, [req.body.sig, req.session.sigId]).then(function(result) {
-            console.log(result);
-
-
-            res.redirect('/signed');
-        }).catch(function(err) {
-            console.log(err);
-        });
-
-    } else {
-        res.redirect('/sign');
     }
 });
 
 app.get('/signers', function (req, res) {
     let list = [];
-    if (req.session.sigId) {
+    if (req.session.user) {
         db.query(`SELECT first , last FROM signatures`).then(function(result){
 
             list = result.rows;
@@ -113,14 +117,79 @@ app.get('/signers', function (req, res) {
     }
 });
 
+app.get('/logout', (req, res) => {
+    req.session = null;
+    res.redirect('/');
+});
 
 app.post('/', (req, res) => {
-    if (req.body.First.length > 0 && req.body.Last.length > 0 && req.body.sig.length > 0) {
-        db.query(`INSERT INTO signatures (first, last, signature) VALUES ($1, $2, $3) RETURNING id`, [req.body.First, req.body.Last, req.body.sig]).then(function(result) {
-            req.session.sigId = result.rows[0].id;
+    if (req.body.First.length > 0 && req.body.Last.length > 0 && req.body.mail.length > 0 && req.body.pass.length > 0) {
+        bcrypt.hashPassword(req.body.pass).then(function(hash){
+            return db.query(`INSERT INTO users (first, last, mail, pass) VALUES ($1, $2, $3, $4) RETURNING id`, [req.body.First, req.body.Last, req.body.mail, hash]);
+        }).then(function(result){
+            req.session.user = {
+                id: result.rows[0].id,
+                first: req.body.First,
+                last: req.body.Last,
+            };
+            res.redirect('/sign');
+        }).catch(function(err) {
+            console.log(err);
+            if (err.code == 23505) {
+                res.render('signup', {
+                    error: 'This Email address is already in use. Please choose a different one!'
+                });
+            } else {
+                res.render('signup', {
+                    error: 'Oops, something went wrong. Please try again!'
+                });
+            }
+        });
+    } else {
+        res.render('signup', {
+            error: 'Oops, something went wrong. Please try again!'
+        });
+    }
+});
 
-            // console.log(result);
+app.post('/login', (req, res) => {
+    if (req.body.mail.length > 0 && req.body.pass.length > 0) {
+        var results;
+        return db.query(`SELECT * FROM users WHERE mail = $1`, [req.body.mail]).then(function(result){
+            results = result;
+            return bcrypt.checkPassword(req.body.pass, result.rows[0].pass);
+        }).then(function(correctPass) {
+            if (correctPass) {
+                req.session.user = {
+                    id: results.rows[0].id,
+                    first: results.rows[0].first,
+                    last: results.rows[0].last
+                };
+                db.query(`SELECT signature, id FROM signatures WHERE user_id = $1`, [results.rows[0].id]).then(function(result){
 
+                    if (result.rows[0].signature && result.rows[0].signature.length > 0) {
+                        req.session.user.sigId = result.rows[0].id;
+                        res.redirect('/signed');
+                    } else { res.redirect('/sign');}
+                });
+            } else {
+                res.render('login', {
+                    error: 'Wrong mail or password, please try again!',
+                });
+            }
+        });
+
+    } else {
+        res.render('login', {
+            error: 'Please fill out both fields!',
+        });
+    }
+});
+
+app.post('/sign', (req, res) => {
+    if (req.body.sig.length > 0) {
+        db.query(`INSERT INTO signatures (user_id, first, last, signature) VALUES ($1, $2, $3, $4) RETURNING id`, [req.session.user.id, req.session.user.first, req.session.user.last, req.body.sig]).then(function(result) {
+            req.session.user.sigId = result.rows[0].id;
             res.redirect('/signed');
         }).catch(function(err) {
             console.log(err);
@@ -128,17 +197,23 @@ app.post('/', (req, res) => {
 
     } else {
         res.render('petition', {
-            error: 'Oops, something went wrong. Please try again!'
+            error: 'Oops, something went wrong. Please try again!',
+            name: req.session.user.first + ' ' + req.session.user.last
         });
     }
 });
 
-app.get('/logout', (req, res) => {
-    req.session = null;
-    res.redirect('/');
+app.post('/signAgain', (req, res) => {
+    if (req.body.sig.length > 0) {
+        db.query(`UPDATE signatures SET signature = $1 WHERE user_id = $2`, [req.body.sig, req.session.user.id]).then(function() {
+            res.redirect('/signed');
+        }).catch(function(err) {
+            console.log(err);
+        });
+    } else {
+        res.redirect('/signAgain');
+    }
 });
-
-
 
 app.get('*', function(req,res) {
     res.redirect('/');
--- signatures.sql
+++ signatures.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS signatures;
 
 CREATE TABLE signatures(
     id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
     first VARCHAR(200) NOT NULL,
     last VARCHAR (200) NOT NULL,
     signature TEXT NOT NULL,
