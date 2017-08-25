deb8bc30ec33cd48501e9b1341ea57f07e84e0ea
dshire <wellereitetclub@gmail.com>
2017-08-25 14:44:02 +0200

    trying to upload the redis version

--- routers/router.js
+++ routers/router.js
@@ -4,8 +4,8 @@ var express = require('express'),
 const spicedPg = require('spiced-pg');
 
 var redis = require('redis');
-var client = redis.createClient({
-    host: process.env.REDIS_URL || 'localhost',
+var client = redis.createClient(process.env.REDIS_URL || {
+    host: 'localhost',
     port: 6379
 });
 client.on('error', function(err) {
